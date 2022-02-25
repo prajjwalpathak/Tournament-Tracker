87aa610d869456cc68f5d08b0b06d03c764c24a6
prajjwalpathak <prajjwalpathak.asm@gmail.com>
2022-02-26 04:49:44 +0530

    Added SQL Connection Module

--- TournamentTracker/TrackerLibrary/GlobalConfig.cs
+++ TournamentTracker/TrackerLibrary/GlobalConfig.cs
@@ -11,22 +11,22 @@ namespace TrackerLibrary
     public static class GlobalConfig
     {
 
-        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
+        public static IDataConnection Connection { get; private set; }
 
-        public static void InitializeConnections(bool database, bool textFiles)
+        public static void InitializeConnections(DatabaseType db)
         {
-            if (database)
+            if (db == DatabaseType.Sql)
             {
-               /// TODO : Create the sql connection
+               // TODO : Create the sql connection
                SqlConnector sql = new SqlConnector();
-                Connections.Add(sql);
+                Connection = sql;
             }
 
-            if (textFiles)
+            else if (db == DatabaseType.TextFile)
             {
-                /// TODO : Create the text connection
+                // TODO : Create the text connection
                 TextConnector text = new TextConnector();
-                Connections.Add(text);
+                Connection = text;
             }
         }
 
